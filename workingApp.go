7f2315fbf7d8cce8b43adc4bfa23f8f260bffc78
Sereja-Lebeda <lserg97@gmail.com>
2025-03-13 16:03:17 +0300

    Params for time of work

--- workingApp.go
+++ workingApp.go
@@ -3,18 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
-func StartWork() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(i, " - прошло времени")
+func StartWork(duration int) {
+	for i := 0; i < duration; i++ {
+		fmt.Println(i+1, "- прошло времени")
 		time.Sleep(1 * time.Second)
 	}
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Использование: program <количество_секунд>")
+		os.Exit(1)
+	}
+
+	duration, err := strconv.Atoi(os.Args[1])
+	if err != nil || duration <= 0 {
+		fmt.Println("Ошибка: укажите положительное число секунд")
+		os.Exit(1)
+	}
+
+	StartWork(duration)
 	fmt.Println("Программа завершится с ошибкой 1")
-	StartWork()
 	os.Exit(1) // Устанавливаем код завершения 1
 }
